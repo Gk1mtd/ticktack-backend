286b2762898bff1514968b3be79dae9257db1104
Philipp Kastl <picastle@posteo.de>
2022-02-16 19:35:28 +0100

    add routes/controller for logout and user delete Co-authored-by: Gk1mtd <Gk1mtd@users.noreply.github.com>

--- modules/auth/controller.js
+++ modules/auth/controller.js
@@ -1,62 +1,86 @@
 // encryption module
-const bcrypt = require("bcryptjs")
+const bcrypt = require('bcryptjs');
 // mongoose user model
-const User = require ("./user.model")
+const User = require('./user.model');
 
 // signup handling
 async function signUp(req, res) {
-    try {
-        // gets user data in req.body
-        const {email, password} = req.body
-        console.log(req.body);
-        // checks for missing data
-        if (!email || !password) {
-            return res.status(400).json({message: "Password and email are required!"})
-        }
-        // checks for existing user
-        const userExists = await User.findOne({email})
-        if (userExists){
-            return res.status(400).json({message: "Email already in use"})
-        }
-        // hashes password
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
-        //creates user in db
-        const user = await User.create({email, password: hashedPassword})
-        // returns user
-        return res.status(200).json({email, id: user.id})
-
-    } catch (error) {
-        return res.status(500).json(error)
+  try {
+    // gets user data in req.body
+    const { email, password } = req.body;
+    console.log(req.body);
+    // checks for missing data
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Password and email are required!' });
     }
+    // checks for existing user
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'Email already in use' });
+    }
+    // hashes password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    //creates user in db
+    const user = await User.create({ email, password: hashedPassword });
+    // returns user
+    return res.status(200).json({ email, id: user.id });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 }
 
 async function login(req, res) {
-    try {
-        // gets user data in req.body
-        const {email, password} = req.body
-        // checks for missing data
-        if (!email || !password) {
-            return res.status(400).json({message: "Password and email are required!"})
-        }
-        // checks for existing user
-        const userExists = await User.findOne({email})
-        if (!userExists){
-            return res.status(400).json({message: "User not found, please Signup"})
-        }
-        // compares saved password with user input
-        const isValidUser = await bcrypt.compare(password, userExists.password)
-        // if no match, returns message
-        if (!isValidUser){
-            return res.status(400).json({message: "Sorry, wrong password."})
-        }
-        // creates user in session
-        req.session.user = {email, id:userExists.id}
-        // return session user to client
-        return res.status(200).json(req.session.user)
-
-    } catch (error) {
-        return res.status(500).json(error)
+  try {
+    // gets user data in req.body
+    const { email, password } = req.body;
+    // checks for missing data
+    if (!email || !password) {
+      return res
+        .status(400)
+        .json({ message: 'Password and email are required!' });
     }
+    // checks for existing user
+    const userExists = await User.findOne({ email });
+    if (!userExists) {
+      return res.status(400).json({ message: 'User not found, please Signup' });
+    }
+    // compares saved password with user input
+    const isValidUser = await bcrypt.compare(password, userExists.password);
+    // if no match, returns message
+    if (!isValidUser) {
+      return res.status(400).json({ message: 'Sorry, wrong password.' });
+    }
+    // creates user in session
+    req.session.user = { email, id: userExists.id };
+    // return session user to client
+    return res.status(200).json(req.session.user);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
 }
-module.exports = {signUp, login}
+
+async function logout(req, res) {
+  try {
+    await req.session.destroy();
+    return res.status(200).json({ message: 'logged out' });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+}
+
+/** deletes user from database by session id, also destroys session => logout */
+async function deleteUser(req, res) {
+  try {
+    const userId = req.session.user.id;
+    const deletedUser = await User.findByIdAndDelete(userId);
+    await req.session.destroy();
+    return res.status(200).json({ message: 'user deleted' });
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+}
+
+module.exports = { signUp, login, logout, deleteUser };
--- modules/auth/router.js
+++ modules/auth/router.js
@@ -1,14 +1,18 @@
 // allow us to use the created controller
-const controller = require("./controller");
+const controller = require('./controller');
 // instanciate router from express
-const router = require("express").Router();
+const router = require('express').Router();
 
 // takes express instance and checks request and links to the right controller
 function authRouter(app) {
   // combines routes with controller, via express.router
-  router.post("/signup", controller.signUp).post("/login", controller.login);
+  router
+    .post('/signup', controller.signUp)
+    .post('/login', controller.login)
+    .post('/logout', controller.logout)
+    .delete('/delete-user', controller.deleteUser);
   // sets baseurl for this auth
-  app.use("/api", router);
+  app.use('/api', router);
 }
 
 module.exports = authRouter;
