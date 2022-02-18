ab28b11c85dafb7940fae9cf1a84acc9c2db068f
Michael Seitz <michael.s.seitz@gmail.com>
2022-02-18 22:06:18 +0100

    does have now the setlist references

--- modules/auth/user.model.js
+++ modules/auth/user.model.js
@@ -1,15 +1,18 @@
-const {Schema, model} = require("mongoose")
+const { Schema, model } = require("mongoose");
 
 const userSchema = new Schema({
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
-})
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  setlists: {
+    type: [{ type: Schema.Types.ObjectId, ref: "Setlist" }],
+  },
+});
 
-module.exports= model("User", userSchema)
+module.exports = model("User", userSchema);
