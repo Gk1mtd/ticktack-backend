edcd4838f4191f46e1e26ccab58bc832cbfe1584
Michael <michael.s.seitz@gmail.com>
2022-08-13 21:58:04 +0200

    cors fix

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
 // create an instance of express server
 const app = require("express")();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 // makes the environments accessable
 require("dotenv").config();
+
 // gets the port from the environment
 const { PORT } = process.env;
+
 // require all configurations for express server instance to use
-const {connectDB} = require("./config/db.config");
-const middlewares = require("./config/middlewares.config");
+const { connectDB } = require("./config/db.config");
 const session = require("./config/session.config");
 const authRouter = require("./modules/auth/router");
 const setlistRouter = require("./modules/setlist/router");
 const songRouter = require("./modules/song/router");
 
+// cors
 const cors = require("cors");
+app.use(cors());
 
 // starts the server with all the configurations
 async function start() {
   try {
     connectDB();
-    middlewares(app);
+
     session(app);
     authRouter(app);
     setlistRouter(app);
     songRouter(app);
-    app.use(cors());
     app.listen(PORT, () => console.log(`Server is running on Port: ${PORT}`));
   } catch (error) {
     console.error(
