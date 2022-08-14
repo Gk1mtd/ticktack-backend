cc972240e3a10485416e6259e0b5215daaf64ddd
Michael <michael.s.seitz@gmail.com>
2022-08-14 14:31:51 +0200

    cors fix

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // middlewares - cors
 const cors = require("cors");
+const origin = process.env.ORIGIN;
 app.use(
   cors({
     origin: origin,
