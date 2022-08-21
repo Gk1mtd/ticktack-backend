9a84b26d99b0d4cd770247fa5331155ff99523d8
michael <michael@meinerseitz.de>
2022-08-21 19:16:19 +0200

    testing

--- app.js
+++ app.js
@@ -10,14 +10,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // middlewares - cors
 const cors = require("cors");
-// const origin = process.env.ORIGIN;
-const origin = [
-  process.env.ORIGIN,
-  `${process.env.ORIGIN}/api/setlist/getall-setlists`,
-  `${process.env.ORIGIN}/api/login`,
-  "https://clickbeat.netlify.app/",
-  "https://clickbeat.netlify.app/login",
-];
+const origin = process.env.ORIGIN;
+// const origin = [
+//   "*",
+//   process.env.ORIGIN,
+//   `${process.env.ORIGIN}/api/setlist/getall-setlists`,
+//   `${process.env.ORIGIN}/api/login`,
+//   "https://clickbeat.netlify.app/",
+//   "https://clickbeat.netlify.app/login",
+// ];
 app.use(
   cors({
     origin: origin,
